48d814f7aba09054287008cbf0a0a8144b6e3289
Marcos Vasconcelos <marcosvasconcelos7901@gmail.com>
2023-12-13 14:04:46 +0000

    fragments para administrador

--- app/src/main/java/pt/ipca/hs/MainAdminActivity.kt
+++ app/src/main/java/pt/ipca/hs/MainAdminActivity.kt
@@ -3,10 +3,32 @@ package pt.ipca.hs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import pt.ipca.hs.databinding.ActivityMainAdminBinding
 
 class MainAdminActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainAdminBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_admin)
+        binding = ActivityMainAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(amaHomeFragment())
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home_ama -> replaceFragment(amaHomeFragment())
+                R.id.perfil_ama -> replaceFragment(amaPerfilFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame_layout_ama,fragment)
+        fragmentTransaction.commit()
     }
 }
